ed3ab21d56a7fe35d997ba54b98ab0ce38860bdb
Julien <sangoku_320@hotmail.fr>
2021-10-21 16:53:03 +0200

    placement mangeur

--- script.js
+++ script.js
@@ -22,3 +22,48 @@ let pacman=[
 [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
+let mangeur={
+    x:6,
+    y:2,
+    direction:0
+}
+
+setInterval(tourDeJeu,500)
+
+function afficheGrille(){
+    document.getElementById("pacman").innerHTML=""
+    for (let i=0; i<22; i++){
+    for(let j=0; j<19; j++){
+        let elem = document.createElement("div")
+        elem.style.gridRowStart = i+1
+        elem.style.gridColumnStart = j+1
+        document.getElementById("pacman").appendChild(elem)
+        if (pacman[i][j] == 0){
+            elem.className = "mur";
+        }
+           else if (pacman[i][j] == 1){
+                elem.className = "sol";
+            }
+                else {
+                    elem.className = "bonbon"
+                }
+        }
+            
+
+    }
+}
+
+function afficheMangeur(){
+    let elem = document.createElement("div")
+    document.getElementById("pacman").appendChild(elem)
+    elem.className = "mangeur"
+    elem.style.gridColumnStart = mangeur.x
+    elem.style.gridRowStart = mangeur.y
+
+}
+function tourDeJeu(){
+    afficheGrille();
+    afficheMangeur();
+}
+
+console.log(afficheMangeur)
